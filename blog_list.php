91d477c9f608d7e6850e8dd9ed0aeca16cb5a1d9
Justin Clark <jclark@cyber.law.harvard.edu>
2011-08-29 12:06:52 -0400

    make blog list in the category page like the regular blog list

--- blog_list.php
+++ blog_list.php
@@ -1,11 +1,11 @@
 <?php if ($posts) : foreach ($posts as $post) : setup_postdata($post); ?>
-<div class="post" id="post-<?php the_ID(); ?>">	
-    <div class="entry">
-        <?php if ( has_post_thumbnail() )the_post_thumbnail(array( 650,999 ),  array( 'alt' => get_the_title(), 'title' => get_the_title(), 'class' => 'singlethumb' )); ?>
-        <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-        <?php $subtitle = get_post_meta($post->ID, 'subtitle', true); if ($subtitle) { ?><div id="subt"> <?php echo $subtitle; ?> </div> <?php } ?>
-        <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-        <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-    </div>
-</div>
+		<div class="post">
+				<div class="entry">
+                	<?php if ( has_post_thumbnail() )the_post_thumbnail(array( 125,125 ),  array( 'alt' => get_the_title(), 'title' => get_the_title(), 'class' => 'thumby' )); ?>
+                	<h2 class="entry-title" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                    <?php $subtitle = get_post_meta($post->ID, 'subtitle', true); if ($subtitle) { ?><div id="subt"> <?php echo $subtitle; ?> </div> <?php } ?>
+					<?php the_excerpt() ?>
+				</div>
+                <a class="more-link" href="<?php the_permalink() ?>" >&raquo;</a>				
+		</div>
 <?php endforeach; endif; ?>
