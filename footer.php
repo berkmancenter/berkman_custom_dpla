68f176d08ac56c7f2ee29d2aa577bc598705d568
Justin Clark <jclark@cyber.law.harvard.edu>
2011-09-28 14:32:06 -0400

    remove the lowest bar from the theme

--- footer.php
+++ footer.php
@@ -7,19 +7,6 @@ if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } e
 }
 ?>	
 	<?php include (TEMPLATEPATH . '/bottombar.php'); ?>
-	<div id="footerbottom">
-        <div id="socialize">
-        	<?php if ($eg_twitter_disable == "No") { ?>
-        	<a href="<?php echo $eg_twitter_url;?>"><img src="<?php bloginfo('template_url'); ?>/images/twitter.png" alt="Follow us on Twitter" /></a>
-            <?php } ?> 
-            <?php if ($eg_rss_disable == "No") { ?>
-            <a href="<?php echo $eg_rss_url;?>"><img src="<?php bloginfo('template_url'); ?>/images/rss.png" alt="Rss Feed" /></a>
-            <?php } ?> 
-            <?php if ($eg_facebook_disable == "No") { ?>
-            <a href="<?php echo $eg_facebook_url;?>"><img src="<?php bloginfo('template_url'); ?>/images/facebook.png" alt="Our Facebook Page" /></a>
-            <?php } ?> 
-        </div>	
-	</div>
 </div>
 <?php wp_footer(); ?>
 </body>
